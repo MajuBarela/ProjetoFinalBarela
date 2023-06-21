168ae9666dcae0b006b00bda378c08d97b666b43
CajuBarela <maria.netto01@etec.sp.gov.br>
2023-06-21 17:54:08 +0000

    Salvando projeto final

--- app/src/main/java/application/controller/ProdutoController.java
+++ app/src/main/java/application/controller/ProdutoController.java
@@ -1,13 +1,11 @@
 package application.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam ;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Produto;
 import application.model.ProdutoRepository;
@@ -23,5 +21,21 @@ public class ProdutoController {
         model.addAttribute("produtos", produtoRepo.findAll());
         return "/produto/list";
     }
-    
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/produto/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(
+        @RequestParam("titulo") String titulo,
+        @RequestParam("descricao") String descricao) {
+            Produto produto = new Produto();
+            produto.setTitulo(titulo);
+            produto.setDescricao(descricao);
+
+            produtoRepo.save(produto);
+            return "redirect:/produto/list";
+        }
 }
